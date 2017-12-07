b22ee975c5a90d1f3f72bb42d1749de477162a33
Jakub Socha <Jakub.Socha@tomtom.com>
2017-12-07 09:18:46 +0100

    Unzipping content

--- stock_exchange.py
+++ stock_exchange.py
@@ -5,6 +5,7 @@ import traceback
 import urllib
 import shutil
 import os
+import zipfile
 
 URL_FOR_STOCK_QUOTES = "http://bossa.pl/pub/ciagle/omega/"
 ZIP_FILE_NAME = "omegacgl.zip"
@@ -19,10 +20,21 @@ def fetch_last_data_file():
     print "Fetch completed"
 
 
+def unzip():
+    print "Unzipping files ..."
+    zip_ref = zipfile.ZipFile(FULL_PATH_TO_SOURCE + ZIP_FILE_NAME, 'r')
+    zip_ref.extractall(FULL_PATH_TO_SOURCE)
+    zip_ref.close()
+    print "Unzipping completed"
+
+
 def create_directory_and_unzip_file():
     if not os.path.exists(FULL_PATH_TO_SOURCE):
         os.makedirs(FULL_PATH_TO_SOURCE)
     shutil.move(ZIP_FILE_NAME, FULL_PATH_TO_SOURCE + ZIP_FILE_NAME)
+    unzip()
+    os.remove(FULL_PATH_TO_SOURCE + ZIP_FILE_NAME)
+
 
 if __name__ == '__main__':
     try:
