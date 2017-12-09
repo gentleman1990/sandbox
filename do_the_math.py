9d9118d3bd51a931f48c6e9f4ec0359c5bdc6b37
Jakub Socha <Jakub.Socha@tomtom.com>
2017-12-09 22:47:22 +0100

    Working oscillators

--- do_the_math.py
+++ do_the_math.py
@@ -3,13 +3,22 @@
 import sys
 import traceback
 from file_operations import prepare_data_for_analysis
+from oscillators import *
+
 
 ALL_COMPANY_DATA = []
+
 
 if __name__ == '__main__':
     try:
         ALL_COMPANY_DATA = prepare_data_for_analysis()
-        print ""
+        for single_company in ALL_COMPANY_DATA:
+            try:
+                print "SMA for " + get_company_name(single_company[0]) + " " + str(calculate_SMA(single_company, 15))
+            except Exception as error:
+                print "Problem with calculation for company %s. Reason %s" % (get_company_name(single_company[0]), error)
+                pass
+
     except Exception as err:
         print("Failed to execute plugin. Reason: %s" % err)
         traceback.print_exc()
--- getters.py
+++ getters.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python
 
 
-def get_company_name(all_company_array):
-    return all_company_array[0]
+def get_company_name(single_company_row):
+    return single_company_row[0]
 
 
-def get_company_date(all_company_array):
-    return all_company_array[1]
+def get_company_date(single_company_row):
+    return single_company_row[1]
 
 
-def get_company_open_price(all_company_array):
-    return all_company_array[2]
+def get_company_open_price(single_company_row):
+    return float(single_company_row[2])
 
 
-def get_company_high_price(all_company_array):
-    return all_company_array[3]
+def get_company_high_price(single_company_row):
+    return float(single_company_row[3])
 
 
-def get_company_low_price(all_company_array):
-    return all_company_array[4]
+def get_company_low_price(single_company_row):
+    return float(single_company_row[4])
 
 
-def get_company_close_price(all_company_array):
-    return all_company_array[5]
+def get_company_close_price(single_company_row):
+    return float(single_company_row[5])
 
 
-def get_company_volume(all_company_array):
-    return all_company_array[6]
+def get_company_volume(single_company_row):
+    return int(single_company_row[6])
+
+
+def get_reverse_array(single_array_company):
+    reversed_array = []
+    iterator = reversed(single_array_company)
+    while iterator.__length_hint__() > 0:
+        reversed_array.append(iterator.next())
+
+    return reversed_array
