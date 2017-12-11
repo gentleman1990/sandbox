d8e1b5449fab27f19b35f794fbee55b956dd44be
Jakub Socha <Jakub.Socha@tomtom.com>
2017-12-11 16:13:24 +0100

    Calucaltions for volume - to be tested

--- oscillators.py
+++ oscillators.py
@@ -20,3 +20,18 @@ def calculate_EMA(array_company, days):
         ema_sum += close_price * (days - single_day)
         weight_sum += (days-single_day)
     return ema_sum/weight_sum
+
+
+def calculate_average_volume(array_company):
+    volume = 0
+    for single_day in range(0, len(array_company), 1):
+        volume += get_company_volume(array_company)
+    return volume/len(array_company)
+
+
+def calculate_average_volume_period(array_company, days):
+    reversed_array = get_reverse_array(array_company)
+    volume = 0
+    for single_day in range(0, days, 1):
+        volume += get_company_volume(reversed_array)
+    return volume/days
