563fbae527d9d065d9834125af3d921208eb1378
Jakub Socha <Jakub.Socha@tomtom.com>
2017-12-15 10:49:09 +0100

    Fix EMA oscillators - finally

--- oscillators.py
+++ oscillators.py
@@ -8,46 +8,35 @@ def calculate_SMA(array_company, days):
     close_price = 0
     for single_day in range(0, days, 1):
         close_price += get_company_close_price(reversed_array[single_day])
-    return close_price/days
+    return close_price / days
+
 
 
 def calculate_EMA(array_company, days):
-    reversed_array = get_reverse_array(array_company)
-    ema_array = prepare_EMA_array(reversed_array, days)
-    return ema_array[days-1]
+    ema_array_from_oldest = prepare_EMA_array(array_company, days)
+    return ema_array_from_oldest[len(array_company)-1]
 
 
-def prepare_EMA_array(reverse_array_company, days):
-    ema_array = []
-    for single_day in range(0, days, 1):
-        close_price = get_company_close_price(reverse_array_company[days-1])
-        a_ratio = 2/float((days+1))
-        if single_day != 0:
-            base = 0
+def prepare_EMA_array(array_company, days):
+    ema_array = {}
+    index = days - 1
+    for single_day in range(index, len(array_company), 1):
+        close_price = get_company_close_price(array_company[single_day])
+        multiplier = 2/float(index)
+        if single_day == index:
+            ema_array[index] = calculate_SMA(array_company, days)
+            continue
         else:
-            base = (1 - a_ratio)*ema_array[single_day-1]
-        ema_array[single_day] = close_price*a_ratio + base
+            ema_day_before = ema_array[single_day - 1]
+        ema_array[single_day] = (close_price - ema_day_before)*multiplier + ema_day_before
     return ema_array
-
-
-# def calculate_EMA(array_company, days):
-#     reversed_array = get_reverse_array(array_company)
-#     ema_sum = 0
-#     weight_sum = 0
-#     for single_day in range(1, days+1, 1):
-#         a = 2/float((single_day+1))
-#         base = (pow((1 - a), single_day))
-#         close_price = get_company_close_price(reversed_array[single_day-1])
-#         ema_sum += (base * close_price)
-#         weight_sum += base
-#     return ema_sum / weight_sum
 
 
 def calculate_average_volume(array_company):
     volume = 0
     for single_day in range(0, len(array_company), 1):
         volume += get_company_volume(array_company[single_day])
-    return volume/len(array_company)
+    return volume / len(array_company)
 
 
 def calculate_average_volume_period(array_company, days):
@@ -55,4 +44,4 @@ def calculate_average_volume_period(array_company, days):
     volume = 0
     for single_day in range(0, days, 1):
         volume += get_company_volume(reversed_array[single_day])
-    return volume/days
+    return volume / days
