68fd7cfab275840a752835a4b25068ced0fd1d61
Jakub Socha <Jakub.Socha@tomtom.com>
2017-12-21 15:02:14 +0100

    Refactor

--- do_the_math.py
+++ do_the_math.py
@@ -3,60 +3,39 @@
 import sys
 import traceback
 from file_operations import prepare_data_for_analysis
-from oscillators import *
-from logger import log_error_to_file
+from typing import *
 
 ALL_COMPANY_DATA = []
-FILTERED_COMPANY_DATA = []
-SMA30 = {}
-EMA15 = {}
-AVG_VOLUME = {}
-UPPER_TRENDING = []
-TYPED_COMPANIES_OSCILLATORS = []
 
 
-def calculate_oscillators():
-    for single_company in ALL_COMPANY_DATA:
-        try:
-            SMA30[get_last_company_name(single_company)] = calculate_SMA(single_company, 30)
-            EMA15[get_last_company_name(single_company)] = calculate_EMA(single_company, 15)
-            AVG_VOLUME[get_last_company_name(single_company)] = calculate_average_volume_period(single_company, 200)
-            FILTERED_COMPANY_DATA.append(single_company)
-        except Exception as error:
-            log_error_to_file("calculate_oscillators", ("Problem with calculation for company %s. Reason %s" % (get_last_company_name(single_company), error)))
-            pass
+def oscillators():
+    print "Starting calculation for oscillators ..."
+    sma30, ema15, avg_vol = calculate_oscillators(ALL_COMPANY_DATA)
+    typed_by_oscillators = type_company_to_invest_by_oscillators(sma30, ema15, avg_vol)
+    print "The calculations for oscillators has been completed"
+    return typed_by_oscillators
 
 
-def type_company_to_invest_by_oscillators():
-    for sc in FILTERED_COMPANY_DATA:
-        company_name = get_last_company_name(sc)
-        sma30 = SMA30[company_name]
-        ema15 = EMA15[company_name]
-        last_volume = get_last_company_volume(sc)
-        if last_volume > 15000 and sma30 > ema15:
-            TYPED_COMPANIES_OSCILLATORS.append(company_name)
-            #print "Potentially company for investment: " + company_name
+def upper_trending():
+    print "Starting calculation for upper trending ..."
+    typed_by_upper_trending = type_company_to_invest_by_trending()
+    print "The calculations for upper trending has been completed"
+    return typed_by_upper_trending
 
 
-def type_company_to_invest_by_trending():
-    for sc in FILTERED_COMPANY_DATA:
-        if check_upper_trending_period(sc, 200):
-            UPPER_TRENDING.append(get_last_company_name(sc))
-            #print "Company with upper trending: " + get_last_company_name(sc)
+def pick_up_for_invest(tbo, tbut):
+    typed = list(set(tbo).intersection(tbut))
+    for t in typed:
+        print "Typed company: " + str(t)
+    print "Typing has been completed"
+
 
 if __name__ == '__main__':
     try:
         ALL_COMPANY_DATA = prepare_data_for_analysis()
-        print "Starting calculation for osillators ..."
-        calculate_oscillators()
-        print "The calculations for oscillators has been completed"
-        print "Starting typing ..."
-        type_company_to_invest_by_oscillators()
-        type_company_to_invest_by_trending()
-        typed = list(set(UPPER_TRENDING).intersection(TYPED_COMPANIES_OSCILLATORS))
-        for t in typed:
-            print "Typed company: " + str(t)
-        print "Typing has been completed"
+        tbo = oscillators()
+        tbut = upper_trending()
+        pick_up_for_invest(tbo, tbut)
     except Exception as err:
         print("Failed to execute plugin. Reason: %s" % err)
         traceback.print_exc()
---
+++ typing.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+from getters import *
+from logger import log_error_to_file
+from oscillators import *
+
+FILTERED_COMPANIES = []
+
+
+def calculate_oscillators(all_company_data):
+    sma30 = {}
+    ema15 = {}
+    avg_vol = {}
+    for single_company in all_company_data:
+        try:
+            sma30[get_last_company_name(single_company)] = calculate_SMA(single_company, 30)
+            ema15[get_last_company_name(single_company)] = calculate_EMA(single_company, 15)
+            avg_vol[get_last_company_name(single_company)] = calculate_average_volume_period(single_company, 200)
+            FILTERED_COMPANIES.append(single_company)
+        except Exception as error:
+            log_error_to_file("calculate_oscillators", ("Problem with calculation for company %s. Reason %s" % (get_last_company_name(single_company), error)))
+            pass
+    return sma30, ema15, avg_vol
+
+
+def type_company_to_invest_by_oscillators(sma30_array, ema15_array, avg_vol_array):
+    typed_companies_oscillators = []
+    for sc in FILTERED_COMPANIES:
+        company_name = get_last_company_name(sc)
+        sma30 = sma30_array[company_name]
+        ema15 = ema15_array[company_name]
+        last_volume = get_last_company_volume(sc)
+        if last_volume > 15000 and sma30 > ema15:
+            typed_companies_oscillators.append(company_name)
+#           print "Potentially company for investment: " + company_name
+    return typed_companies_oscillators
+
+
+def type_company_to_invest_by_trending():
+    upper_trending = []
+    for sc in FILTERED_COMPANIES:
+        if check_upper_trending_period(sc, 200):
+            upper_trending.append(get_last_company_name(sc))
+#           print "Company with upper trending: " + get_last_company_name(sc)
+    return upper_trending
